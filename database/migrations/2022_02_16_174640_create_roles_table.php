57b7e2e7c707be88e5d8c052b07ab30dcd4b81ba
Jason Limas <45784146+jasonlimas@users.noreply.github.com>
2022-02-17 04:56:37 +1100

    Created roles table and updated user table column

--- database/migrations/2022_02_16_174640_create_roles_table.php
+++ database/migrations/2022_02_16_174640_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUsernameAndRolesToUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class AddUsernameAndRolesToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('username');
-            $table->string('role');
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->timestamps();
         });
     }
 
@@ -26,8 +27,6 @@ class AddUsernameAndRolesToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(['username', 'role']);
-        });
+        Schema::dropIfExists('roles');
     }
 }
