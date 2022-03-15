7649b88931bac9f404bbc107b243d1739bad846a
Jason Limas <45784146+jasonlimas@users.noreply.github.com>
2022-03-15 14:25:08 +1100

    Moved scripts section for FilePond file input

--- resources/views/quote/create.blade.php
+++ resources/views/quote/create.blade.php
@@ -89,19 +89,5 @@
                 @endauth
             </div>
         </div>
-        @section('scripts')
-            <script>
-                const inputElement = document.querySelector('input[id="attachment"]');
-                const pond = FilePond.create(inputElement);
-                FilePond.setOptions({
-                    server: {
-                        url: '/upload',
-                        headers: {
-                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                        }
-                    }
-                });
-            </script>
-        @endsection
     </div>
 @endsection
