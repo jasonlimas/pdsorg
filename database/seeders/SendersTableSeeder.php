0c199dfcbe1593838c31bc79d2c339a7018ac6c1
Jason Limas <jasonandrea14@gmail.com>
2022-03-22 18:24:10 +1100

    Updated senders table seeder

--- database/seeders/SendersTableSeeder.php
+++ database/seeders/SendersTableSeeder.php
@@ -18,11 +18,11 @@ class SendersTableSeeder extends Seeder
         DB::table('senders')->insert([
             'name' => 'PT. XDC Indonesia',
             'address' => 'KETAPANG BUSINESS CENTER BLOK D2-D3',
-            'bank_info' => [[
+            'bank_info' => [
                 'institution' => 'BCA',
                 'accountName' => 'XDC INDONESIA PT',
                 'accountNumber' => '1458888808',
-            ]],
+            ],
         ]);
     }
 }
