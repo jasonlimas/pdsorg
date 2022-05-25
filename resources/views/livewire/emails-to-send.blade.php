8a76e3115189ad9f9534459fe64fc9add87e233c
Jason Limas <jasonandrea14@gmail.com>
2022-05-25 12:01:42 +0700

    Modify email input table to make it work with livewire

--- resources/views/livewire/emails-to-send.blade.php
+++ resources/views/livewire/emails-to-send.blade.php
@@ -8,34 +8,38 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td class="align-middle text-center">
-                    <p class="font-semibold">1</p>
-                </td>
-                <td>
-                    <div class="flex flex-wrap">
-                        <div class="w-full">
-                            <input required
-                                class="shadow appearance-none border rounded w-full p-2 mx-1 text-gray-700 leading-none focus:outline-none focus:shadow-outline"
-                                name="TODO" placeholder="email@mail.com" </div>
+            @foreach ($emails as $index => $email)
+                <tr>
+                    <td class="align-middle text-center">
+                        <p class="font-semibold">{{ $index + 1 }}</p>
+                    </td>
+                    <td>
+                        <div class="flex flex-wrap">
+                            <div class="w-full">
+                                <input required
+                                    class="shadow appearance-none border rounded w-full p-2 mx-1 text-gray-700 leading-none focus:outline-none focus:shadow-outline"
+                                    name="emails[{{ $index }}]" placeholder="email@mail.com"
+                                    wire:model="emails.{{ $index }}">
+                            </div>
                         </div>
-                    </div>
-                </td>
+                    </td>
 
-                <!-- Delete button -->
-                <td class="text-center">
-                    <form>
-                        @csrf
-                        <button type="button" class="hover:bg-gray-300 p-2 rounded">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-red-500" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                            </svg>
-                        </button>
-                    </form>
-                </td>
-            </tr>
+                    <!-- Delete button -->
+                    <td class="text-center">
+                        <form>
+                            @csrf
+                            <button type="button" class="hover:bg-gray-300 p-2 rounded"
+                                wire:click.prevent="removeEmail({{ $index }})">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-red-500" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 
@@ -49,7 +53,7 @@
     <div class="flex flex-wrap justify-center">
         <button
             class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 m-2 rounded focus:outline-none focus:shadow-outline w-full"
-            wire:click.prevent="addTermsCondition" type="button">
+            wire:click.prevent="addEmail" type="button">
             + Add Row
         </button>
     </div>
