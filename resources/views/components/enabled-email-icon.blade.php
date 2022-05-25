1232071bac20a54447602ecb28515f7fd65c132d
Jason Limas <jasonandrea14@gmail.com>
2022-05-25 14:13:43 +0700

    Remove email button confirmation prompt in quote list page

--- resources/views/components/enabled-email-icon.blade.php
+++ resources/views/components/enabled-email-icon.blade.php
@@ -1,8 +1,4 @@
-<button
-    onclick="return confirm('Are you sure you want to send this quote to the client\'s email?')"
-    type="submit"
-    class="hover:bg-gray-300 p-1 rounded"
-    title="Email">
+<button type="submit" class="hover:bg-gray-300 p-1 rounded" title="Email">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-700" viewBox="0 0 20 20" fill="currentColor">
         <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
         <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
