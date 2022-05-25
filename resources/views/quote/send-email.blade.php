e3f90c047912679330926909b02023ebd93d47b9
Jason Limas <jasonandrea14@gmail.com>
2022-05-25 12:01:00 +0700

    Added send email button in send email page

--- resources/views/quote/send-email.blade.php
+++ resources/views/quote/send-email.blade.php
@@ -12,9 +12,22 @@
     <div class="flex justify-center">
         <div class="w-8/12">
             <div class="bg-gray-100 p-6 rounded-lg">
+                <!-- List of recipients -->
                 <form action="" method="">
                     @livewire('emails-to-send')
                 </form>
+
+                <!-- Send email button -->
+                <div class="flex justify-center space-x-2 text-center">
+                    <form action="{{ route('quotes.email', $quote) }}" method="POST">
+                        @csrf
+                        <button
+                            class="bg-indigo-500 hover:bg-indigo-700 transition-colors duration-200 text-white font-bold py-2 px-4 rounded-lg"
+                            type="submit">
+                            Send Email
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
