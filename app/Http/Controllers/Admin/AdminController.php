c6e3424eef13c52c2bdf88d0888d18b9f93a237e
Jason Limas <jasonandrea14@gmail.com>
2022-06-21 14:14:28 +0700

    Update tax function

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\Division;
 use App\Models\Sender;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -16,11 +17,27 @@ class AdminController extends Controller
         $users = User::paginate(5, ['*'], 'users');
         $senders = Sender::paginate(5, ['*'], 'senders');
         $divisions = Division::paginate(5, ['*'], 'divisions');
+        $tax = DB::table('app_settings')->where('setting_name', 'tax')->first()->setting_value;
 
         return view('user.admin.index', [
             'users' => $users,
             'senders' => $senders,
             'divisions' => $divisions,
+            'tax' => $tax,
         ]);
+    }
+
+    public function updateTax(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'tax' => 'required|numeric|min:0|max:100',
+        ]);
+
+        // Update the tax in the database
+        DB::table('app_settings')->where('setting_name', 'tax')->update(['setting_value' => $request->tax]);
+
+        // Redirect to the admin page
+        return redirect()->route('admin')->with('status', 'Settings updated successfully');
     }
 }
